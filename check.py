46a826c36f28a318acbd62899bc48f5fbb6a2062
Rajakumar <rajakumarofficial@gmail.com>
2017-06-22 22:46:15 +0530

    i have made some changes
    I have wrapped the main login inside a function so that it is easy to understand.

--- check.py
+++ check.py
@@ -1,10 +1,12 @@
 # Positve-Negative
 """check whether a number is positive or negative or zero its very simple"""
-
-x=int(raw_input("Enter the Number :" ))
-if x>0:
+def chknum(x):
+   if x>0:
    print ("Positive Number")
 elif x<0:
    print ("Negative Number")
 else:
    print("Zero")
+   
+x=int(raw_input("Enter the Number :" ))
+chknum(x)
